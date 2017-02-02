4446c369f1d692797a065d09bc1a192131245e77
Raul <rauleto_7@hotmail.com>
2017-02-02 17:25:25 -0600

    Practica 1 Variables

---
+++ Practicas/Variables.java
@@ -0,0 +1,32 @@
+public class Variables{
+
+	static String var5;
+
+	public static void main(String[] args){
+		byte var=127;
+		short var1=32767;
+		int var2=2147483647;
+		long var3=9223372036854775807L;
+		char var4='2';
+		String var6= "hola java";
+
+
+		
+
+
+
+	System.out.println("Impresion de Variables 1");
+	
+	System.out.println("byte = "+var);
+	System.out.println("short = "+var1);
+	System.out.println("int = "+var2);
+	System.out.println("long = "+var3);
+	System.out.println("char = "+var4);
+	System.out.println("String var ="+var5);
+	System.out.println("String var ="+var6);
+
+	
+	
+
+	}
+}
---
+++ Practicas/Variables2.java
@@ -0,0 +1,30 @@
+public class Variables2{
+
+	    static byte var;
+	 	static short var1;
+		static int var2;
+		static long var3;
+		static double var4;
+		static char var6;
+		static boolean var7;
+		static String var8;
+
+		
+
+	public static void main (String[] args){
+       System.out.println("Variables2");
+
+        
+    System.out.println("byte var = "+var);
+	System.out.println("short var = "+var1);
+	System.out.println("int var = "+var2);
+	System.out.println("long var = "+var3);
+	System.out.println("double var = "+var4);
+	System.out.println("Char var ="+var6);
+	System.out.println("boolean var: "+var7);
+	System.out.println("String var ="+var8);
+
+
+
+	}
+}
