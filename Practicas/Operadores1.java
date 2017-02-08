a11aefe56279d40be3db2b49cd4c4f517ebe9d16
Raul <rauleto_7@hotmail.com>
2017-02-08 01:00:14 -0600

    Operadores2

---
+++ Practicas/Operadores1.java
@@ -0,0 +1,27 @@
+public class Operadores1{
+	public static void main(String[] args){
+        
+    int a_1=2;
+    int a_2=7;
+
+
+    System.out.println("Operadores y Estructuras de Control 2");
+    System.out.println("\n");
+    System.out.println("**Even or Odd**");
+    System.out.println("\n");
+
+    if(a_1%2==0){
+    System.out.println("input is " +a_1+", then output should be: 'Even' ");
+    }else{
+    	System.out.println("input is " +a_1 +", then output should be: 'Odd' ");
+    }
+
+    if(a_2%2==0){
+    System.out.println("input is " +a_2+", then output should be: 'Even' ");
+    }else{
+    	System.out.println("input is " +a_2+", then output should be: 'Odd' ");
+    }
+
+
+	}
+}
