dfaabb998e3631bb9b109795356a12223f886668
Raul <rauleto_7@hotmail.com>
2017-02-07 21:16:12 -0600

    Practica3

--- Practicas/Operadores.java
+++ Practicas/Operadores.java
@@ -3,24 +3,24 @@ public class Operadores{
 
 	public static void main(String[] args){
 		int result =0;
-		byte a_1=1,a_2=2;
+		
 
 
 
 	System.out.println("Operadores y Estructuras de Control");
 	System.out.println("\n");
 
-	result=a_1+a_2;
+	result+=3;
     System.out.println("result is now  "+result);
-    result=result-1;
+    result-=1;
     System.out.println("result is now  "+result);
-    result=result+2;
+    result*=2;
     System.out.println("result is now  "+result);
-    result=result-2;
+    result-=2;
     System.out.println("result is now  "+result);
-    result=result*5;
+    result*=5;
     System.out.println("result is now  "+result);
-    result=result-7;
+    result-=7;
     System.out.println("result is now  "+result);
 
 	}
