4b737d56189e54979c29e5d86eecf975b14235ad
Raul <rauleto_7@hotmail.com>
2017-02-15 20:25:07 -0600

    Practica4 2-3

---
+++ Practicas/Practica4_1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+public class Practica4_1{
+
+    public static void printCapitalized(String n){
+    	String m;
+    	m=n.toUpperCase();
+    	System.out.println(m);
+    }
+	public static void main(String[] args) 
+
+	{
+
+		System.out.println("Ejercicio 2 Practica 4");
+		System.out.println("\n");
+
+		String n="Hola mundo java";
+
+		printCapitalized(n);
+	}
+}
---
+++ Practicas/Practica4_2.java
@@ -0,0 +1,20 @@
+public class Practica4_2{
+	public static void main(String[] args){
+		String word="Hello, my friend";
+		int a=word.indexOf(","); //n-1
+		String word1;
+		word1 =word.substring(0,a);
+		
+
+System.out.println("\n");
+
+System.out.println("Ejercicio 3 Practica 4");
+
+System.out.println(word1);
+
+
+
+
+
+	}
+}
